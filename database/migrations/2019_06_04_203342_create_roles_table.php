41b9de70f51da4a14a7ce1468e4ae52efcb71368
David Callizaya <davidcallizaya@gmail.com>
2021-05-23 18:11:54 -0400

    Fix role column type

--- database/migrations/2019_06_04_203342_create_roles_table.php
+++ database/migrations/2019_06_04_203342_create_roles_table.php
@@ -15,7 +15,7 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->text('role');
+            $table->string('role');
             $table->string('status')->default('ACTIVE');
             $table->timestamps();
 
