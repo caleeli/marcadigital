9807abf4dab9418a0fbb6a996f1587f348edd263
David Callizaya <davidcallizaya@gmail.com>
2021-05-23 20:56:01 -0400

    Add toolbar for items in home

--- resources/js/modules/Home.vue
+++ resources/js/modules/Home.vue
@@ -25,7 +25,11 @@
       </b-card-body>
     </b-card>
     <div class="d-flex flex-row justify-content-center">
-      <b-card v-for="item in items" :key="`item-${item.id}`" class="mt-2 ml-1 mr-1">
+      <b-card
+        v-for="item in items"
+        :key="`item-${item.id}`"
+        class="mt-2 ml-1 mr-1"
+      >
         <dl>
           <dt>
             <b-link :to="`certificate/${item.id}`">
@@ -38,6 +42,26 @@
             {{ item.attributes.date }}
           </dd>
         </dl>
+        <div class="text-right">
+          <b-button-group class="mx-1">
+            <b-button
+              variant="outline-secondary"
+              :to="`certificate/${item.id}`"
+            >
+              <i class="fas fa-pen"></i>
+            </b-button>
+            <b-button
+              variant="outline-secondary"
+              :href="`/credential/${item.id}`"
+              target="_blank"
+            >
+              <i class="far fa-eye"></i>
+            </b-button>
+            <b-button variant="outline-secondary" @click="dropItem(item)">
+              <i class="fas fa-trash"></i>
+            </b-button>
+          </b-button-group>
+        </div>
       </b-card>
     </div>
   </panel>
@@ -51,6 +75,20 @@ export default {
     return {
       items: this.$api.user[`${window.userId}/certifications`].array(),
     };
+  },
+  methods: {
+    dropItem(item) {
+      this.$bvModal
+        .msgBoxConfirm(this.__("Are you sure to delete this item?"))
+        .then((value) => {
+          this.$api.certification.delete(item.id).then(() => {
+            this.refresh();
+          });
+        });
+    },
+    refresh() {
+      this.$api.user[`${window.userId}/certifications`].refresh(this.items);
+    },
   },
 };
 </script>
